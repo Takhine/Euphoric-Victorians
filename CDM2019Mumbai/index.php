c40c0dd6460c72744b65bed38345c1185bae9b41
Aniketh <anikethnair97@gmail.com>
2019-08-14 11:13:23 +0530

    Layout
    Login form layout | File structure

--- CDM2019Mumbai/index.php
+++ CDM2019Mumbai/index.php
@@ -10,7 +10,25 @@
     <div class="container">
       <div class="my-4">
       <h1>Main page</h1>
-      <h2>Login</h2>
+      <form>
+  <div class="form-group row">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Username</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputUsername3" placeholder="Username">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-12">
+      <button type="submit" class="btn btn-primary pull-right">Proceed</button>
+    </div>
+  </div>
+</form>
       
       </div>  
     </div>
